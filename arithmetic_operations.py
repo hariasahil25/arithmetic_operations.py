bdf8c4125f746879a619e825a4578e027c863663
hariasahil25 <hariyasahil1025@gmail.com>
2026-05-02 19:46:34 +0530

    Add arithmetic operations program in Python

---
+++ arithmetic_operations.py
@@ -0,0 +1,26 @@
+# Program to perform different arithmetic operations in Python
+
+# Taking two numbers
+num1 = 20
+num2 = 5
+
+# Arithmetic operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+floor_division = num1 // num2
+modulus = num1 % num2
+exponent = num1 ** num2
+
+# Displaying results
+print("First Number:", num1)
+print("Second Number:", num2)
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Floor Division:", floor_division)
+print("Modulus:", modulus)
+print("Exponent:", exponent)
